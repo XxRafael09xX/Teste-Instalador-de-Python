1438460785fc8cfc4de7420edefdbc078bef5eba
Rafael Silva <rafajogos14@yahoo.com.br>
2019-04-23 01:28:43 -0300

    1

--- Teste Instalador de Python/Program.cs
+++ Teste Instalador de Python/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Microsoft.Win32;
 namespace Teste_Instalador_de_Python {
     static class Program {
         /// <summary>
@@ -11,9 +11,27 @@ namespace Teste_Instalador_de_Python {
         /// </summary>
         [STAThread]
         static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            // Application.EnableVisualStyles();
+            //  Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+                       
+
+            RegistryKey registro = Registry.CurrentUser;
+
+            string[] b = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Python\\PythonCore").GetSubKeyNames();
+
+            registro = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Python\\PythonCore\\" + b[0] + "\\InstallPath");
+
+            string valor = registro.GetValue("ExecutablePath").ToString();
+
+            MessageBox.Show(valor);
+
+
+                       
         }
+
     }
+
+
 }
+
